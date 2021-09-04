dbcd0b11df9d6d2ec37c712ff5e80ec576ea4a87
Vedanta Nahata <82261949+VedantaNahata@users.noreply.github.com>
2021-09-04 12:59:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function display(e) {
+    document.getElementById("output-display").value += e;
+}
+function solve() {
+    let input_number = document.getElementById("output-display").value;
+    let final_output= eval(input_number);
+    document.getElementById("output-display").value = final_output;
+}
+function vedanta() {
+   let answer = document.getElementById("output-display");
+   answer.value = "0";
+}
